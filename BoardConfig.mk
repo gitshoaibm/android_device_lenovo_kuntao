a82f8522969602c3cfc29dffb2c438f3e110e270
ShoaibM <shoaib.gcp@gmail.com>
2019-05-21 07:14:31 +0000

    kuntao: adapt for PE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,7 +125,8 @@ TARGET_KERNEL_CONFIG := lineageos_kuntao_defconfig
 TARGET_KERNEL_SOURCE := kernel/lenovo/msm8953
 
 # Lineage Hardware
-JAVA_SOURCE_OVERLAYS := org.potato.hardware|$(DEVICE_PATH)/potatohw|**/*.java
+#JAVA_SOURCE_OVERLAYS := org.potato.hardware|$(DEVICE_PATH)/potatohw|**/*.java
+JAVA_SOURCE_OVERLAYS := org.pixelexperience.keydisabler|$(DEVICE_PATH)/keydisabler|**/*.java
 
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true

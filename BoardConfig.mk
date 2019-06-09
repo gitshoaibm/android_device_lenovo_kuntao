4fb8f868927386d36a384f7665d935b4fda84628
ShoaibM <shoaib.gcp@gmail.com>
2019-06-09 10:58:41 +0000

    kuntao: allow never allows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,7 @@ VENDOR_SECURITY_PATCH := 2017-11-01
 # SELinux
 include device/qcom/sepolicy/Android.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
